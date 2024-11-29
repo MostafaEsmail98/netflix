8ef09d89850b0847322cc44af193e74c8901d1d8
Mostafa Esmail <mostafaelshrbeeny98@gmail.com>
2024-11-29 15:22:27 +0200

    putting blocProvider of all movie on home page and trigger function

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:netflix/core/utils/app_styles.dart';
 import 'package:netflix/core/utils/custom_space_height.dart';
+import 'package:netflix/features/home/presentation/manager/all_movies_cubit.dart';
 import 'package:netflix/features/home/presentation/widgets/app_bar_of_home.dart';
 import '../widgets/list_of_movies.dart';
 import '../widgets/movie_card.dart';
@@ -11,25 +13,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.sizeOf(context).width * .03),
-      child: CustomScrollView(
-        slivers: [
-          CustomAppBar.appBarOfHome(context),
-          SliverToBoxAdapter(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const MovieCard(),
-                    Text(
-                      "Movies",
-                      style: AppStyles.textSemiBold24(context),
-                    ),
-                    const CustomSpaceHeight(height: .01),
-                  ])),
-          const ListOfMovies()
-        ],
+    return BlocProvider(
+      create: (context) => AllMoviesCubit()..getAllMovies(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery
+                .sizeOf(context)
+                .width * .03),
+        child: CustomScrollView(
+          slivers: [
+            CustomAppBar.appBarOfHome(context),
+            SliverToBoxAdapter(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const MovieCard(),
+                      Text(
+                        "Movies",
+                        style: AppStyles.textSemiBold24(context),
+                      ),
+                      const CustomSpaceHeight(height: .01),
+                    ])),
+            const ListOfMovies()
+          ],
+        ),
       ),
     );
   }
