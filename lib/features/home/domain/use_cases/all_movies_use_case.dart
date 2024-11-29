c88dd5b61fbd7dbc39aca3aca73438676cdf8f6f
Mostafa Esmail <mostafaelshrbeeny98@gmail.com>
2024-11-29 15:05:51 +0200

    create all movie use cases and call function

---
+++ lib/features/home/domain/use_cases/all_movies_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:netflix/core/errors/exceptions.dart';
+import 'package:netflix/features/home/domain/entities/all_movies_entity.dart';
+import 'package:netflix/features/home/domain/repositories/all_movie_repo.dart';
+
+class AllMoviesUseCase {
+  AllMovieRepo allMovieRepo ;
+
+  AllMoviesUseCase({required this.allMovieRepo});
+
+  Future<Either<Failure,AllMoviesEntity>>call()async{
+    return await allMovieRepo.fetchAllMovies();
+  }
+}
