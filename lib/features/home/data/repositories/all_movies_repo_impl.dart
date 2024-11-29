b899cecd8b5a77663f62ddbb7de8e11602c5bd58
Mostafa Esmail <mostafaelshrbeeny98@gmail.com>
2024-11-29 15:00:36 +0200

    create all movie repo impl

---
+++ lib/features/home/data/repositories/all_movies_repo_impl.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:netflix/core/errors/exceptions.dart';
+import 'package:netflix/features/home/data/data_sources/api/remote_all_movie.dart';
+import 'package:netflix/features/home/domain/entities/all_movies_entity.dart';
+import 'package:netflix/features/home/domain/repositories/all_movie_repo.dart';
+
+class AllMoviesRepoImpl extends AllMovieRepo {
+  RemoteAllMovie remoteAllMovie ;
+
+  AllMoviesRepoImpl({required this.remoteAllMovie});
+
+  @override
+  Future<Either<Failure, AllMoviesEntity>> fetchAllMovies() async{
+  return await remoteAllMovie.fetchAllMovie() ;
+  }
+
+}
