5665b126977090d2308a666588f5f3ef9fc000dc
Mostafa Esmail <mostafaelshrbeeny98@gmail.com>
2024-11-29 13:01:33 +0200

    finish splash page update routes add logo

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -2,13 +2,18 @@
 import 'package:go_router/go_router.dart';
 import 'package:netflix/features/details/presentation/pages/details_page.dart';
 import 'package:netflix/features/mainScreen/main_screen.dart';
+import 'package:netflix/features/splash/presentation/pages/splash_page.dart';
 
 abstract class AppRouter {
-  static const String home = "/";
+  static const String splash = "/";
+  static const String home = "/home";
   static const String details = "/details";
 
   static final routes = GoRouter(routes: [
     GoRoute(
+      path: splash,
+      builder: (context, state) => const SplashPage(),
+    ),    GoRoute(
       path: home,
       builder: (context, state) => const MainScreen(),
     ),
