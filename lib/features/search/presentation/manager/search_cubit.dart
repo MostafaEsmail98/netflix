7a6b99cf980b99dae4d59257c1fc90622ce08b50
Mostafa Esmail <mostafaelshrbeeny98@gmail.com>
2024-11-29 18:43:10 +0200

    call function in search cubit

--- lib/features/search/presentation/manager/search_cubit.dart
+++ lib/features/search/presentation/manager/search_cubit.dart
@@ -1,13 +1,24 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix/core/params/params.dart';
+import 'package:netflix/core/utils/services_locator.dart';
 import 'package:netflix/features/search/domain/entities/search_of_movies_entity.dart';
-
+import 'package:netflix/features/search/domain/use_cases/search_use_cases.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
   SearchCubit() : super(SearchInitial());
 
-  getSearchResult(){
+  TextEditingController typeSearch = TextEditingController();
 
+  getSearchResult() async {
+    emit(SearchLoading());
+    var response = await getIt
+        .get<SearchUseCases>()
+        .call(SearchParams(search: typeSearch.text));
+
+    response.fold((failure) => emit(SearchFailure(failure.message)),
+        (result) => emit(SearchSuccessful(result)));
   }
 }
