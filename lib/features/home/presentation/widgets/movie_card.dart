0c8bdd335a46448a381585186bd8d905055a6415
Mostafa Esmail <mostafaelshrbeeny98@gmail.com>
2024-11-29 03:16:18 +0200

    fix error in movie card

--- lib/features/home/presentation/widgets/movie_card.dart
+++ lib/features/home/presentation/widgets/movie_card.dart
@@ -10,51 +10,55 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Card(
-          child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(12)),
-              child: Image.asset(Assets.imagesLargeImage)),
-        ),
-        Column(
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        height: MediaQuery.sizeOf(context).height * .4,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
           children: [
-            Text(
-              "Film Name",
-              style:
-              AppStyles.textBold11(context).copyWith(fontSize: 32),
+            Card(
+              child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  child: Image.asset(Assets.imagesLargeImage)),
             ),
-            Text(
-              "Summary",
-              style: AppStyles.textRegular14(context),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                  vertical: MediaQuery.sizeOf(context).height * .02),
-              width: MediaQuery.sizeOf(context).width * .7,
-              height: MediaQuery.sizeOf(context).height * .05,
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                  color: Colors.white),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.play_arrow,
-                    color: Colors.black,
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Film Name",
+                  style: AppStyles.textBold11(context).copyWith(fontSize: 24),
+                ),
+                Text(
+                  "Summary",
+                  style: AppStyles.textRegular14(context),
+                ),
+                Container(
+                  width: MediaQuery.sizeOf(context).width * .5,
+                  height: MediaQuery.sizeOf(context).height * .05,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      color: Colors.white),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.play_arrow,
+                        color: Colors.black,
+                      ),
+                      Text(
+                        "Play",
+                        style: AppStyles.textSemiBold16(context)
+                            .copyWith(color: Colors.black),
+                      )
+                    ],
                   ),
-                  Text(
-                    "Play",
-                    style: AppStyles.textSemiBold16(context)
-                        .copyWith(color: Colors.black),
-                  )
-                ],
-              ),
-            )
+                )
+              ],
+            ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
